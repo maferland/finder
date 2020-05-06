2fc69c57365093e82c3f3ec047756fb3b1f6df10
Marc-Antoine Ferland <maf.ferland@gmail.com>
2020-05-06 17:15:04 -0400

    :sparkles: Add basic netlify functions

---
+++ src/functions/get-markers/get-markers.js
@@ -0,0 +1,17 @@
+exports.handler = async (event, context) => {
+  const claims = context.clientContext && context.clientContext.user
+
+  if (!claims) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        data: 'NOT ALLOWED'
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify([])
+  }
+}
